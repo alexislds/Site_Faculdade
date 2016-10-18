85e97b3e2b96e3f8b6c71f5d545a6a34e8d71b78
Alexis Santos <alexis.luiz.dias@gmail.com>
2016-10-18 14:50:11 -0200

    rolar arrumado

--- js/rolar.js
+++ js/rolar.js
@@ -3,7 +3,7 @@ var $getBotoes = $getBody.querySelectorAll('.btn-efeito');
 
 $getBotoes[0].addEventListener('click', function(el){
   el.preventDefault();
-  rolar('.servicos', 5);
+  rolar('.servicos', 100);
 });
 $getBotoes[1].addEventListener('click', function(el){
   el.preventDefault();
@@ -34,46 +34,46 @@ $getBotoes[7].addEventListener('click', function(el){
   rolar('.trabalheconosco', 5);
 });
 
-function rolar(el, duracao) {
-  var $getEl = document.querySelector(el);
-  var $getTamanhoBody = document.body.offsetHeight*4;
-  var posicaoEl = $getEl.offsetTop;
-  var posicaoAtual = window.pageYOffset;
-  var velocidade = 15;
+function rolar(el, duracao, velocidade) {
+  var velocidade = velocidade || 15;
+  
+  var $getEl       = document.querySelector(el);
+  var posicaoEl    = parseInt($getEl.offsetTop);
+  var posicaoAtual = parseInt(window.pageYOffset);
   var distancia;
   var andar;
-
+    
   var rolar = setInterval(function(){
     var distancia = posicaoEl - posicaoAtual;
-    var velocidade = 15;
     var andarDown = posicaoAtual + velocidade;
     var andarUp = posicaoAtual - velocidade;
-
-    if (posicaoAtual > $getTamanhoBody) {
-      clearInterval(rolar);
-    }
-    else if (posicaoAtual < posicaoEl) {
-      if (distancia >= 20) {
+    
+    if (posicaoAtual < posicaoEl) {
+      
+      if (distancia >= velocidade) {
         scrollTo(0, andarDown);
-        posicaoAtual = window.pageYOffset;
+        
+        posicaoAtual = parseInt(window.pageYOffset);
       }
       else {
-        velocidade = 1;
-        andarDown = posicaoAtual + velocidade;
-        scrollTo(0, andarDown);
-        posicaoAtual = window.pageYOffset;
+        scrollTo(0, posicaoEl);
+        
+        posicaoAtual = parseInt(window.pageYOffset);
+        
+        clearInterval(rolar);
       }
     }
     else if (posicaoAtual > posicaoEl) {
-      if (distancia <= 20) {
+      
+      if (distancia <= velocidade) {
         scrollTo(0, andarUp);
-        posicaoAtual = window.pageYOffset;
+        posicaoAtual = parseInt(window.pageYOffset);
       }
       else {
         velocidade = 1;
         andarUp = posicaoAtual - velocidade;
         scrollTo(0, andarUp);
-        posicaoAtual = window.pageYOffset;
+        posicaoAtual = parseInt(window.pageYOffset);
       }
     }
     else if (posicaoAtual == posicaoEl) {
