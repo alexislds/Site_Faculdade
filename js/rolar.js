b49e0d7df0f7200e61c03c8874224e1c33e01741
Alexis Santos <alexis.luiz.dias@gmail.com>
2016-10-21 21:51:31 -0200

    Faculdade: final

--- js/rolar.js
+++ js/rolar.js
@@ -3,7 +3,7 @@ var $getBotoes = $getBody.querySelectorAll('.btn-efeito');
 
 $getBotoes[0].addEventListener('click', function(el){
   el.preventDefault();
-  rolar('.servicos', 100);
+  rolar('.servicos', 5);
 });
 $getBotoes[1].addEventListener('click', function(el){
   el.preventDefault();
@@ -36,35 +36,35 @@ $getBotoes[7].addEventListener('click', function(el){
 
 function rolar(el, duracao, velocidade) {
   var velocidade = velocidade || 15;
-  
+
   var $getEl       = document.querySelector(el);
   var posicaoEl    = parseInt($getEl.offsetTop);
   var posicaoAtual = parseInt(window.pageYOffset);
   var distancia;
   var andar;
-    
+
   var rolar = setInterval(function(){
     var distancia = posicaoEl - posicaoAtual;
     var andarDown = posicaoAtual + velocidade;
     var andarUp = posicaoAtual - velocidade;
-    
+
     if (posicaoAtual < posicaoEl) {
-      
+
       if (distancia >= velocidade) {
         scrollTo(0, andarDown);
-        
+
         posicaoAtual = parseInt(window.pageYOffset);
       }
       else {
         scrollTo(0, posicaoEl);
-        
+
         posicaoAtual = parseInt(window.pageYOffset);
-        
+
         clearInterval(rolar);
       }
     }
     else if (posicaoAtual > posicaoEl) {
-      
+
       if (distancia <= velocidade) {
         scrollTo(0, andarUp);
         posicaoAtual = parseInt(window.pageYOffset);
